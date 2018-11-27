b53d34554248a3b466bd933a1a9e1fbd6288a97b
Rodrigo Fernando Velazquez Cruz <rodrigo_velazquez99@ciencias.unam.mx>
2018-11-27 14:23:59 -0600

    Cambios al signup

--- research_network/database/forms.py
+++ research_network/database/forms.py
@@ -1,15 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import New_User
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('email','password')
+from .models import People
 
 class UserProfileInfoForm(forms.ModelForm):
     class Meta():
-        model = New_User
-        fields = ('username','email','academic_level', 'degree',
+        model = People
+        fields = ('name','email','academic_level', 'degree','state',
                   'personal_telephone', 'institute', 'subinstitute')
 
