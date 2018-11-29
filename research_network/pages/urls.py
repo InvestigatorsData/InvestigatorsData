f23c70328ced8f079f5c08269753ddd8589b4295
Romina Caracal <rominacaracal@vl25h52.fciencias.unam.mx>
2018-11-29 15:43:15 -0600

    Arreglando problemas de compatibilidad

--- research_network/pages/urls.py
+++ research_network/pages/urls.py
@@ -10,12 +10,9 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/',
     database.views.activate, name='activate'),
     path('login/', database.views.user_login, name='login'),
-<<<<<<< HEAD
     path('loginadmin/', database.views.user_login, name='loginadmin'),
     url(r'logout/$', database.views.user_logout, name='logout'),
-=======
     path('logout/', database.views.user_logout, name='logout'),
->>>>>>> 4d50ecf0a22c222e237f4cf2936d49d8fd7f7896
     path('home/', views.HomePageView.as_view(), name='home'),
     path('profile/', views.ProfilePageView.as_view(), name='profile'),
     path('profileM/', views.ProfileModifyPageView.as_view(), name='profileM'),
