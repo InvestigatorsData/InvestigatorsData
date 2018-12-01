a952e978b7927f9207070e5e6dc5630980ffb828
Rodrigo Fernando Velázquez Cruz <rodrigo_velazquez99@ciencias.unam.mx>
2018-11-30 22:39:55 -0600

    Cambios de seguridad del registro de usuario

--- research_network/pages/urls.py
+++ research_network/pages/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('login/', database.views.user_login, name='login'),
     path('loginadmin/', database.views.user_login, name='loginadmin'),
     url(r'logout/$', database.views.user_logout, name='logout'),
-    path('logout/', database.views.user_logout, name='logout'),
     path('home/', views.HomePageView.as_view(), name='home'),
     path('profile/', views.ProfilePageView.as_view(), name='profile'),
     path('profileM/', views.ProfileModifyPageView.as_view(), name='profileM'),
